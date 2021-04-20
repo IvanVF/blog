b91b1f12288d165a1021d35b841e871bc8c27e80
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-20 15:48:32 +0700

    Добавил /home в  @GetMapping

--- src/main/java/com/fprojects/blog/controllers/MainController.java
+++ src/main/java/com/fprojects/blog/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home(Model model) {
         //model.addAttribute("title", "Main Page");
         return "home";
