c5f0e79063cde2e9ed02c204ed5a1337211c84b9
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-20 12:11:20 +0700

    Максимально упростил

--- src/main/java/com/fprojects/blog/controllers/MainController.java
+++ src/main/java/com/fprojects/blog/controllers/MainController.java
@@ -3,13 +3,15 @@ package com.fprojects.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
+    @ResponseBody
     public String home(Model model) {
-        model.addAttribute("title", "Main Page");
+        //model.addAttribute("title", "Main Page");
         return "home";
     }
 }
