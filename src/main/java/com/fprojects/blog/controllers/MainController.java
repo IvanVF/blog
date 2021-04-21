6a31b54314af1852e62bd4f65d575fb76ee499a6
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-21 22:08:13 +0700

    Добавил keystore

--- src/main/java/com/fprojects/blog/controllers/MainController.java
+++ src/main/java/com/fprojects/blog/controllers/MainController.java
@@ -2,9 +2,7 @@ package com.fprojects.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -12,6 +10,6 @@ public class MainController {
     @RequestMapping("/home")
     public String home(Model model) {
         //model.addAttribute("title", "Main Page");
-        return "home";
+        return "index";
     }
 }
