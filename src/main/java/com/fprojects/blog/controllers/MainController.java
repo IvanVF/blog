4d0ba297891e40f8efadd95f8f902d25c6bd6417
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-21 22:01:33 +0700

    Заменил на  @RequestMapping

--- src/main/java/com/fprojects/blog/controllers/MainController.java
+++ src/main/java/com/fprojects/blog/controllers/MainController.java
@@ -3,12 +3,13 @@ package com.fprojects.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
+    @RequestMapping("/home")
     public String home(Model model) {
         //model.addAttribute("title", "Main Page");
         return "home";
