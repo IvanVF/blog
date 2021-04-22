ca6bf2fea16b91e765c18bc9a048637d25745fb3
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-22 15:55:56 +0700

    Добавил блог и о нас, ссылки на них и вынес футер с header в отдельный файл

--- src/main/java/com/fprojects/blog/controllers/MainController.java
+++ src/main/java/com/fprojects/blog/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.fprojects.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,11 @@ public class MainController {
     public String home(Model model) {
         //model.addAttribute("title", "Main Page");
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Страница про нас");
+        return "about";
     }
 }
