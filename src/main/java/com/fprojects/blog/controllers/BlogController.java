c372d6edb96f9626125274bbec9e06ad84478912
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-23 23:31:46 +0700

    Добавил контроллер добавления статей

--- src/main/java/com/fprojects/blog/controllers/BlogController.java
+++ src/main/java/com/fprojects/blog/controllers/BlogController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
@@ -19,5 +21,21 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blogMain";
     }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blogAdd";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,
+                              @RequestParam String anons,
+                              @RequestParam String full_text,
+                              Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
 
 }
--- src/main/java/com/fprojects/blog/entitys/Post.java
+++ src/main/java/com/fprojects/blog/entitys/Post.java
@@ -1,6 +1,8 @@
 package com.fprojects.blog.entitys;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import java.util.UUID;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Post {
 
     @Id
@@ -23,4 +26,10 @@ public class Post {
     private String fullText;
 
     private int views;
+
+    public Post(String title, String anons, String fullText) {
+        this.title = title;
+        this.anons = anons;
+        this.fullText = fullText;
+    }
 }
