4d5ed4c2bf0e7dc1c13c71ba88e5ac642aef45d3
IvanVF <fedinivanvladimirovich@mail.ru>
2021-04-24 22:23:01 +0700

    Добавил полный просмотр статей

--- src/main/java/com/fprojects/blog/controllers/BlogController.java
+++ src/main/java/com/fprojects/blog/controllers/BlogController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class BlogController {
@@ -15,6 +20,9 @@ public class BlogController {
     @Autowired
     private PostRepository postRepository;
 
+    /**
+     *Страница блога со всеми статьями
+     */
     @GetMapping("/blog")
     public String blogMain(Model model) {
         Iterable<Post> posts = postRepository.findAll();
@@ -22,19 +30,40 @@ public class BlogController {
         return "blogMain";
     }
 
+    /**
+     *Страница добавления статьи в блог
+     */
     @GetMapping("/blog/add")
     public String blogAdd(Model model) {
         return "blogAdd";
     }
 
+    /**
+     *Добавить статью в блог
+     */
     @PostMapping("/blog/add")
-    public String blogPostAdd(@RequestParam String title,
+    public String blogAdd(@RequestParam String title,
                               @RequestParam String anons,
                               @RequestParam String full_text,
                               Model model) {
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    /**
+     *Получить одну статью из блога
+     */
+    @GetMapping("/blog/{id}")
+    public String blogGet(@PathVariable(value = "id") UUID id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blogDetails";
     }
 
 
